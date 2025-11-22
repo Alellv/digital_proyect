b21816529a487af2697bde7d07947497e1e2845e
alellv <llanovesga@gmail.com>
2025-11-22 15:05:25 -0500

    Codigo verilog sqrt

---
+++ Calculadora/sqrt/sqrt.v
@@ -0,0 +1,99 @@
+module sqrt (
+    input              reset,
+    input              clk,
+    
+    input              init,
+    output reg         done,
+
+    input      [15:0]  op_A,      // numero a encontrar raiz
+    output reg [15:0]  result     // resultado
+);
+
+parameter START          = 3'b000;
+parameter LOAD           = 3'b001;
+parameter SHIFT          = 3'b010;
+parameter TEST           = 3'b011;
+parameter CHECK_END      = 3'b100;
+parameter END            = 3'b101;
+
+reg [2:0] state;
+
+reg [31:0] A;          // registro raiz expandido
+reg [15:0] result;          // root parcial
+reg [15:0] comparador;
+reg [15:0] residuo;
+
+reg [4:0]  count;
+
+initial begin
+    result = 0;
+    done   = 0;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset) begin
+        state   <= START;
+        done    <= 0;
+        result  <= 0;
+    end else begin
+        case(state)
+
+        START: begin
+            done  <= 0;
+            result <= 0;
+            count <= 8;          // 16 bits 8 iteraciones
+            if(init)
+                state <= LOAD;
+            else
+                state <= START;
+        end
+        
+        LOAD: begin
+            A <= {op_A,16'b0};
+            residuo <= 0;
+            result <= 0;
+            done <= 0;
+            state <= SHIFT;
+        end
+
+        SHIFT: begin // bajar los siguientes 2 bits
+            residuo <= {residuo[13:0], A[31:30]};   
+            A <= {A[29:0],2'b00};
+            state <= TEST;
+        end
+
+        TEST: begin
+            comparador = (result << 1) + 1;
+            if(residuo >= comparador) begin
+                residuo <= residuo - comparador;
+                result <= (result << 1) | 1;
+            end
+            else begin
+                result <= (result << 1);
+            end
+            state <= CHECK_END;
+        end
+
+        CHECK_END: begin
+            if(count == 0) begin
+                result <= result;
+                state <= END;
+            end else begin
+                count <= count - 1;
+                state <= SHIFT;
+            end
+        end
+
+        END: begin
+            done <= 1;
+            state <= START;
+        end
+        
+        default: state <= START;
+
+        endcase
+    end
+end
+
+endmodule
