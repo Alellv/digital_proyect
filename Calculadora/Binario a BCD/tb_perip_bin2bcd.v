ab645045ff2cca0f14fcc9982c7f7dda11335798
Duvs22 <79936640+Duvs22@users.noreply.github.com>
2025-11-23 18:13:02 -0500

    Update tb_perip_bin2bcd.v

--- Calculadora/Binario a BCD/tb_perip_bin2bcd.v
+++ Calculadora/Binario a BCD/tb_perip_bin2bcd.v
@@ -2,7 +2,6 @@
 
 module tb_perip_bin2bcd;
 
-    // Señales del Bus
     reg clk;
     reg reset;
     reg [4:0] addr;
@@ -11,10 +10,8 @@ module tb_perip_bin2bcd;
     reg wr;
     wire [31:0] dout;
     
-    // Variable para lectura segura
     reg [31:0] dato_leido_safe;
 
-    // Instancia del Periférico (Device Under Test)
     perip_bin2bcd uut (
         .clk(clk),
         .reset(reset),
@@ -25,10 +22,8 @@ module tb_perip_bin2bcd;
         .dout(dout)
     );
 
-    // Reloj
     always #5 clk = ~clk;
 
-    // --- TAREAS DEL BUS (Con el fix del #1 incluido) ---
     
     task escribir_bus(input [4:0] direccion, input [31:0] dato);
         begin
@@ -53,7 +48,7 @@ module tb_perip_bin2bcd;
             wr = 0;
             @(posedge clk);
             
-            #1; // Retardo clave para evitar lectura de ceros
+            #1; 
             
             dato_leido_safe = dout; 
             cs = 0;
@@ -61,7 +56,6 @@ module tb_perip_bin2bcd;
         end
     endtask
 
-    // --- PROCESO DE PRUEBA ---
     initial begin
         $dumpfile("sim_perip_bin.vcd");
         $dumpvars(0, tb_perip_bin2bcd);
@@ -71,18 +65,12 @@ module tb_perip_bin2bcd;
 
         $display("--- INICIO TEST PERIFERICO BIN2BCD ---");
 
-        // 1. ESCRIBIR EL DATO BINARIO (255 = 0xFF)
-        // Dirección 0x04
         $display("[CPU] Enviando Binario 255 (0xFF)...");
         escribir_bus(5'h04, 32'h000000FF);
 
-        // 2. DAR START
-        // Dirección 0x08
         $display("[CPU] Dando Start...");
         escribir_bus(5'h08, 32'h00000001);
 
-        // 3. ESPERAR (POLLING DONE)
-        // Dirección 0x14
         $display("[CPU] Esperando...");
         leer_bus(5'h14);
         while (dato_leido_safe == 0) begin
@@ -91,11 +79,8 @@ module tb_perip_bin2bcd;
         end
         $display("[CPU] Done recibido!");
 
-        // 4. LEER RESULTADO BCD
-        // Dirección 0x10
         leer_bus(5'h10);
         
-        // El resultado BCD de 255 debe verse como 0x255
         $display("Resultado BCD: %h (Esperado: 00000255)", dato_leido_safe);
 
         #50;
