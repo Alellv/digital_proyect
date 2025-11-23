ae02541b02bedac6afdecaf209837e8c69e8d439
Duvs22 <79936640+Duvs22@users.noreply.github.com>
2025-11-23 17:40:31 -0500

    Update bcd_sub_block.v

--- Calculadora/BCD a Binario/bcd_sub_block.v
+++ Calculadora/BCD a Binario/bcd_sub_block.v
@@ -2,6 +2,5 @@ module bcd_sub_block (
     input wire [3:0] in_val,
     output wire [3:0] out_val
 );
-    // Si es >= 8, restamos 3. Si no, pasa igual.
     assign out_val = (in_val >= 8) ? (in_val - 4'd3) : in_val;
 endmodule
